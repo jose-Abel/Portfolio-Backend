d09842d21d062f5206fcd0af18a337cef492a133
jose-abel <guest1@guest1s-MacBook-Pro.local>
2022-10-18 14:36:49 -0400

    Create the categories_controller test

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
 
   before_action :set_project, only: %i[ show edit update destroy ]
 
-  # GET /projects or /projects.json
+  # GET /
   def index
     ctx = Project::Index.call
 
@@ -12,65 +12,34 @@ class ProjectsController < ApplicationController
     json_response(ctx.projects, 200)
   end
 
-  # GET /projects/1 or /projects/1.json
+  # GET /1
   def show
   end
 
-  # GET /projects/new
+  # GET /new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
+  # GET /1/edit
   def edit
   end
 
-  # POST /projects or /projects.json
+  # POST /
   def create
-    @project = Project.new(project_params)
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    
   end
 
-  # PATCH/PUT /projects/1 or /projects/1.json
+  # PATCH/PUT /1
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to project_url(@project), notice: "Project was successfully updated." }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    
   end
 
-  # DELETE /projects/1 or /projects/1.json
+  # DELETE /1
   def destroy
-    @project.destroy
-
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def project_params
-      params.require(:project).permit(:title, :description, :see_code, :live_version, images: [])
-    end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,8 +1,23 @@
 require "test_helper"
 
-class CategoriesControllerTest < ActionDispatch::IntegrationTest
+class CategoriesControllerTest < ActionController::TestCase
   setup do
     @category = Category.create(name: "HTML")
+  end
+
+  setup do
+    @request.headers['Accept'] = "application/json"
+    @request.headers['Content-Type'] = "application/json"
+
+    @category = Category.create(name: "HTML")
+  end
+
+  test "should get index" do
+    get :index, :format => "json"
+    json_response = JSON.parse(response.body)
+    
+    assert_response :success    
+    assert_equal json_response.first["name"], "HTML"
   end
 
 end
